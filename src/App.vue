b13055ada0d12685ff57f914ef2d5c8abdafff4d
coderken <jk0910948659@gmail.com>
2022-05-18 12:25:05 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <!-- <Computed /> -->
-  <watchValue />
+  <!-- <watchValue /> -->
+  <button @click="showStatus = !showStatus">顯示/隱藏</button>
+  <LifeCycle v-if="showStatus" />
 </template>
 
 <script>
 // import Computed from './components/computed.vue';
 // import watchCourse from './components/watchCourse.vue';
-import watchValue from './components/watchValue.vue';
+// import watchValue from './components/watchValue.vue';
+import LifeCycle from './components/lifeCycle.vue';
+
+import { ref } from 'vue';
 export default {
   name: 'app',
-  components: { watchValue },
+  components: { LifeCycle },
+  setup() {
+    let showStatus = ref(true);
+
+    return { showStatus };
+  },
 };
 </script>
 
